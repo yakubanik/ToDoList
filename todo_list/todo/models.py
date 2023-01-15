b2aae3c935b9086d2d88fec7d3425c77c0c3aea6
Nik <yakubamykyta@gmail.com>
2023-01-15 18:36:35 +0200

    add model for todo

--- todo_list/todo/models.py
+++ todo_list/todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    edited = models.DateTimeField(auto_now=True)
+    completed = models.DateTimeField(null=True)
