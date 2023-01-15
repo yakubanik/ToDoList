c86943c7732de5e6f7321757a67fa06995187b03
Nik <yakubamykyta@gmail.com>
2023-01-15 18:45:14 +0200

    fix todo model

--- todo_list/todo/models.py
+++ todo_list/todo/models.py
@@ -5,4 +5,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     edited = models.DateTimeField(auto_now=True)
-    completed = models.DateTimeField(null=True)
+    completed = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.title} ({self.edited})'
