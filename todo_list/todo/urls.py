798861f5fcecc4eca6125e059fa291cec443d822
Nik <yakubamykyta@gmail.com>
2023-01-15 19:23:02 +0200

    add view todos page

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('todos/', views.view_todos, name='index'),
 ]
--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import Todo
 
 
 def index(request):
     return render(request, 'todo/index.html')
+
+
+def view_todos(request):
+    todos = get_list_or_404(Todo)
+    return render(request, 'todo/view_todos.html', {'todos': todos})
