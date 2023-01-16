5c5c9ee3ce8bff3d45c2c6307876a1924d9d129a
Nik <yakubamykyta@gmail.com>
2023-01-16 12:25:12 +0200

    add sign up page

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('todos/', views.view_todos, name='index'),
+    path('sign_up/', views.sign_up, name='sign_up'),
 ]
--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, get_list_or_404
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
+from django.shortcuts import render, get_list_or_404, redirect
 from .models import Todo
 
 
@@ -9,3 +13,23 @@ def index(request):
 def view_todos(request):
     todos = get_list_or_404(Todo)
     return render(request, 'todo/view_todos.html', {'todos': todos})
+
+
+def sign_up(request):
+    if request.method == 'GET':
+        return render(request, 'todo/sign_up.html', {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                username = request.POST['username']
+                password = request.POST['password1']
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+                login(request, user)
+                return redirect('index')
+            except IntegrityError:
+                return render(request, 'todo/sign_up.html', {'form': UserCreationForm,
+                                                             'error_message': 'Login is already taken'})
+        else:
+            return render(request, 'todo/sign_up.html', {'form': UserCreationForm,
+                                                         'error_message': 'Passwords do not match'})
