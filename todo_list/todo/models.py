f1686acbc3b32f4fca1d3bed8e01c2381b93223e
Nik <yakubamykyta@gmail.com>
2023-01-16 19:18:42 +0200

    some changes in models

--- todo_list/todo/models.py
+++ todo_list/todo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
@@ -6,6 +7,7 @@ class Todo(models.Model):
     description = models.TextField(blank=True)
     edited = models.DateTimeField(auto_now=True)
     completed = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
-        return f'{self.title} ({self.edited})'
+        return f'{self.author} - {self.title}'
