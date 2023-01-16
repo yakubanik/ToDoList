a5332200e9c822001d1974d27fb83c9c6a714436
Nik <yakubamykyta@gmail.com>
2023-01-16 12:50:29 +0200

    add sign in page

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('todos/', views.view_todos, name='index'),
     path('sign_up/', views.sign_up, name='sign_up'),
+    path('sign_in/', views.sign_in, name='sign_in'),
 ]
--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.db import IntegrityError
 from django.shortcuts import render, get_list_or_404, redirect
 from .models import Todo
@@ -33,3 +33,18 @@ def sign_up(request):
         else:
             return render(request, 'todo/sign_up.html', {'form': UserCreationForm,
                                                          'error_message': 'Passwords do not match'})
+
+
+def sign_in(request):
+    if request.method == 'GET':
+        return render(request, 'todo/sign_in.html', {'form': AuthenticationForm})
+    else:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return render(request, 'todo/sign_in.html', {'form': AuthenticationForm,
+                                                         'error_message': 'Incorrect username and/or password'})
+        else:
+            login(request, user)
+            return redirect('index')
