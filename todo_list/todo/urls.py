3345f6580de22048205e4c2e16c21effe5c81591
Nik <yakubamykyta@gmail.com>
2023-01-16 13:37:40 +0200

    add logout, fix one bug

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('todos/', views.view_todos, name='index'),
+    path('todos/', views.view_todos, name='todos'),
     path('sign_up/', views.sign_up, name='sign_up'),
     path('sign_in/', views.sign_in, name='sign_in'),
+    path('sign_out/', views.sign_out, name='sign_out'),
 ]
--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.db import IntegrityError
 from django.shortcuts import render, get_list_or_404, redirect
 from .models import Todo
@@ -48,3 +48,8 @@ def sign_in(request):
         else:
             login(request, user)
             return redirect('index')
+
+
+def sign_out(request):
+    logout(request)
+    return redirect('index')
